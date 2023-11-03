3aac6506e634ae293f3ecad008751f0ff8afbdf2
KODI-lil <68896917+KODI-lil@users.noreply.github.com>
2023-11-03 14:53:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function checkText1() {
+    rebus = document.getElementById('rebus1').value;
+      if (rebus == "компьютер" || rebus == "КОМПЬЮТЕР" || rebus == "Компьютер")
+      {document.getElementById('rebus1').value="Верно";}
+      else 
+      document.getElementById('rebus1').value="Ошибка";
+    }
+  function checkText2() {
+    rebus = document.getElementById('rebus2').value;
+      if (rebus == "монитор" || rebus == "МОНИТОР" || rebus =="Монитор")
+      {document.getElementById('rebus2').value="Верно";}
+      else 
+       document.getElementById('rebus2').value="Ошибка";
+    }
+  function checkText3() {
+    rebus = document.getElementById('rebus3').value;
+      if (rebus == "блок питания" || rebus == "БЛОК ПИТАНИЯ" || rebus == "Блок питания")
+      {document.getElementById('rebus3').value="Верно";}
+      else 
+      document.getElementById('rebus3').value="Ошибка";
+    }
+  function checkText4() {
+    rebus = document.getElementById('rebus4').value;
+      if (rebus == "клавиатура" || rebus == "КЛАВИАТУРА" || rebus =="Клавиатура")
+      {document.getElementById('rebus4').value="Верно";}
+      else 
+      document.getElementById('rebus4').value="Ошибка";
+    }
+  function checkText5() {
+    rebus = document.getElementById('rebus5').value;
+      if (rebus == "программа" || rebus == "ПРОГРАММА" || rebus =="Программа")
+      {document.getElementById('rebus5').value="Верно";}
+      else 
+      document.getElementById('rebus5').value="Ошибка";
+    }
